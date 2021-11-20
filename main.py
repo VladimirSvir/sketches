cc18263d67866e59028237184159e4a6f35a243e
VladimirSvir <74258376+VladimirSvir@users.noreply.github.com>
2021-11-20 20:05:07 +0300

    Creating the another foundation for the application

--- main.py
+++ main.py
@@ -2,15 +2,37 @@ import pygame as pg
 
 res = WIDTH, HEIGTH = 1600, 900
 FPS = 120
+COL_STAR = 1000
+
+class Star:
+    def __init__(self, app):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+class Starfield:
+    def __init__(self, app):
+        self.stars = [Star(app) for _ in range(COL_STAR)]
+
+    def run(self):
+        [star.update() for star in self.stars]
+        [star.draw() for star in self.stars]
 
 class App:
     def __init__(self):
         self.screen = pg.display.set_mode(res)
         self.clock = pg.time.Clock()
+        self.starfield = Starfield(self)
 
     def run(self):
         while True:
             self.screen.fill('black')
+            self.starfield.run()
 
             pg.display.flip()
             [exit() for event in pg.event.get() if event.type == pg.QUIT]
