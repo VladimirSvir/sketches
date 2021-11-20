b81d2ee173c30ce6b6d37ed713bf682d345e4d01
VladimirSvir <74258376+VladimirSvir@users.noreply.github.com>
2021-11-20 19:23:20 +0300

    Creating the foundation for the application

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import pygame as pg
+
+res = WIDTH, HEIGTH = 1600, 900
+FPS = 120
+
+class App:
+    def __init__(self):
+        self.screen = pg.display.set_mode(res)
+        self.clock = pg.time.Clock()
+
+    def run(self):
+        while True:
+            self.screen.fill('black')
+
+            pg.display.flip()
+            [exit() for event in pg.event.get() if event.type == pg.QUIT]
+            self.clock.tick(FPS)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = App()
+    app.run()
