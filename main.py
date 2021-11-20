86d406fe06462e4937eb9a597a8a093fe730205d
VladimirSvir <74258376+VladimirSvir@users.noreply.github.com>
2021-11-20 21:03:29 +0300

    a full-fledged picture of flying "stars"

--- main.py
+++ main.py
@@ -5,26 +5,26 @@ import math
 
 res = WIDTH, HEIGTH = 1600, 900
 FPS = 120
-COL_STAR = 100
+COL_STAR = 1500
 
 vec2, vec3 = pg.math.Vector2, pg.math.Vector3
 CENTER = vec2(WIDTH // 2, HEIGTH // 2)
-COLORS = 'red green blue orange purple cyan white'.split()
-Z_DISTANCE = 30
+COLORS = 'white grey red green blue orange purple cyan'.split()
+Z_DISTANCE = 100
 
 
 class Star:
     def __init__(self, app):
         self.screen = app.screen
         self.pos3d = self.get_pos3d()
-        self.vel = random.uniform(0.005, 0.175)
+        self.vel = random.uniform(0.005, 0.25)
         self.color = random.choice(COLORS)
         self.screen_pos = vec2(0, 0)
         self.size = 5
 
-    def get_pos3d(self):
+    def get_pos3d(self, scale_pos=45):
         angel = random.uniform(0, 2 * math.pi)
-        radius = random.randrange(HEIGTH)
+        radius = random.randrange(HEIGTH // scale_pos, HEIGTH) * scale_pos
         x = radius * math.sin(angel)
         y = radius * math.cos(angel)
         return vec3(x, y, Z_DISTANCE)
@@ -34,7 +34,7 @@ class Star:
         self.pos3d = self.get_pos3d() if self.pos3d.z < 1 else self.pos3d
 
         self.screen_pos = vec2(self.pos3d.x, self.pos3d.y) / self.pos3d.z + CENTER
-        self.size = Z_DISTANCE / self.pos3d.z
+        self.size = (Z_DISTANCE - self.pos3d.z) / (0.9 * self.pos3d.z)
 
     def draw(self):
         pg.draw.circle(self.screen, self.color, self.screen_pos, self.size)
@@ -46,6 +46,7 @@ class Starfield:
 
     def run(self):
         [star.update() for star in self.stars]
+        self.stars.sort(key=lambda star: star.pos3d.z, reverse=True)
         [star.draw() for star in self.stars]
 
 class App:
