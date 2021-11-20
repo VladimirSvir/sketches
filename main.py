4a3671c106ddd5f6e4bc5a0b14bdef7366748f7d
VladimirSvir <74258376+VladimirSvir@users.noreply.github.com>
2021-11-20 20:32:24 +0300

    creating flying "stars" on a black screen

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 import pygame as pg
+import random
+import math
+
 
 res = WIDTH, HEIGTH = 1600, 900
 FPS = 120
-COL_STAR = 1000
+COL_STAR = 100
+
+vec2, vec3 = pg.math.Vector2, pg.math.Vector3
+CENTER = vec2(WIDTH // 2, HEIGTH // 2)
+COLORS = 'red green blue orange purple cyan white'.split()
+Z_DISTANCE = 30
+
 
 class Star:
     def __init__(self, app):
-        pass
+        self.screen = app.screen
+        self.pos3d = self.get_pos3d()
+        self.vel = random.uniform(0.005, 0.175)
+        self.color = random.choice(COLORS)
+        self.screen_pos = vec2(0, 0)
+        self.size = 5
+
+    def get_pos3d(self):
+        angel = random.uniform(0, 2 * math.pi)
+        radius = random.randrange(HEIGTH)
+        x = radius * math.sin(angel)
+        y = radius * math.cos(angel)
+        return vec3(x, y, Z_DISTANCE)
 
     def update(self):
-        pass
+        self.pos3d.z -= self.vel
+        self.pos3d = self.get_pos3d() if self.pos3d.z < 1 else self.pos3d
+
+        self.screen_pos = vec2(self.pos3d.x, self.pos3d.y) / self.pos3d.z + CENTER
+        self.size = Z_DISTANCE / self.pos3d.z
 
     def draw(self):
-        pass
+        pg.draw.circle(self.screen, self.color, self.screen_pos, self.size)
 
 
 class Starfield:
