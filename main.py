a373f91742667219bf77535a29707021eee0a824
VladimirSvir <74258376+VladimirSvir@users.noreply.github.com>
2021-11-20 21:16:46 +0300

    well done

--- main.py
+++ main.py
@@ -4,27 +4,30 @@ import math
 
 
 res = WIDTH, HEIGTH = 1600, 900
-FPS = 120
-COL_STAR = 1500
+FPS = 60
+COL_STAR = 2000
 
 vec2, vec3 = pg.math.Vector2, pg.math.Vector3
 CENTER = vec2(WIDTH // 2, HEIGTH // 2)
-COLORS = 'white grey red green blue orange purple cyan'.split()
-Z_DISTANCE = 100
+COLORS = 'white grey blue cyan skyblue purple magenta'.split()
+# Z_DISTANCE = 40
+Z_DISTANCE = 140
+# ALPHA = 120
+ALPHA = 50
 
 
 class Star:
     def __init__(self, app):
         self.screen = app.screen
         self.pos3d = self.get_pos3d()
-        self.vel = random.uniform(0.005, 0.25)
+        self.vel = random.uniform(0.25, 0.75)
         self.color = random.choice(COLORS)
         self.screen_pos = vec2(0, 0)
-        self.size = 5
+        self.size = 10
 
-    def get_pos3d(self, scale_pos=45):
+    def get_pos3d(self, scale_pos=35):
         angel = random.uniform(0, 2 * math.pi)
-        radius = random.randrange(HEIGTH // scale_pos, HEIGTH) * scale_pos
+        radius = random.randrange(HEIGTH // 7, HEIGTH) * scale_pos
         x = radius * math.sin(angel)
         y = radius * math.cos(angel)
         return vec3(x, y, Z_DISTANCE)
@@ -34,7 +37,7 @@ class Star:
         self.pos3d = self.get_pos3d() if self.pos3d.z < 1 else self.pos3d
 
         self.screen_pos = vec2(self.pos3d.x, self.pos3d.y) / self.pos3d.z + CENTER
-        self.size = (Z_DISTANCE - self.pos3d.z) / (0.9 * self.pos3d.z)
+        self.size = (Z_DISTANCE - self.pos3d.z) / (0.5 * self.pos3d.z)
 
     def draw(self):
         pg.draw.circle(self.screen, self.color, self.screen_pos, self.size)
@@ -52,12 +55,15 @@ class Starfield:
 class App:
     def __init__(self):
         self.screen = pg.display.set_mode(res)
+        self.alpha_surface = pg.Surface(res)
+        self.alpha_surface.set_alpha(ALPHA)
         self.clock = pg.time.Clock()
         self.starfield = Starfield(self)
 
     def run(self):
         while True:
-            self.screen.fill('black')
+            # self.screen.fill('black')
+            self.screen.blit(self.alpha_surface, (0, 0))
             self.starfield.run()
 
             pg.display.flip()
